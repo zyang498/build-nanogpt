25dfa938cdbddb71fb41f8982899a465610cdb34
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-06-07 02:17:20 +0000

    add tiny shakespeare and create an example little batch out of it

--- train_gpt2.py
+++ train_gpt2.py
@@ -169,22 +169,35 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
 
-num_return_sequences = 5
-max_length = 30
-
-# model = GPT.from_pretrained('../gpt2')
-model = GPT(GPTConfig()) ## random model initialization by PyTorch default constructor
-model.eval()
-model.to(device)
-
-# prefix tokens
+# get a data batch
 import tiktoken
 enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("Hello, I'm a language model,") ## tiktokenizer.vercel.app
+with open('input.txt', 'r') as f:
+    text = f.read()
+text = text[:1000]
+tokens = enc.encode(text)
+B, T = 4, 32
+buf = torch.tensor(tokens[:B*T + 1])
+x = buf[:-1].view(B, T)
+y = buf[1:].view(B, T)
+
+# model = GPT.from_pretrained('../gpt2')
+# get logits
+model = GPT(GPTConfig()) ## random model initialization by PyTorch default constructor
+model.to(device)
+logits = model(x)
+
+print(logits.shape)
+import sys; sys.exit(0)
+
+# prefix tokens
+model.eval()
+num_return_sequences = 5
+max_length = 30
+tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
-## x = tokens.to('cuda') ## x is the idx in the forward function input
-x = tokens.to(device)
+x = tokens.to(device) ## x is the idx in the forward function input
 
 # generate! right now x is (B, T) where B = 5, T = 8
 # set the seed to 42
